df370b25b30f552a46bd2f9e8f859fb7b447b6c3
NormanPirk <norman.pirk@gmail.com>
2019-11-14 11:03:22 +0200

    Sain vastusevalikud nii, et nad on suvalises järjekorras. Lisasin mõne koha peale kommentaarid, et oleks veidi lihtsam aru saada.

--- KM_vol2.py
+++ KM_vol2.py
@@ -1,4 +1,4 @@
-from random import randint, sample
+from random import randint, sample, shuffle
 from tkinter import *
 import tkinter as tk
 from tkinter import ttk
@@ -16,35 +16,43 @@ def failist_sõnastik(failinimi):
 def küsi_küsimus(sõnastik):
     testiaken = tk.Toplevel(aken)
     testiaken.title('Test')
-    vastusevalikud = []
+
+#    siin saab järjendi (vastusevariandid), kus on 4 vastusevarianti, millest kolm on valed ja üks õige. 
+    võtmed = []
     for k in sõnastik:
-        vastusevalikud.append(k)
-    
-    if 'astakuks ' in vastusevalikud:
+        võtmed.append(k)
+    koopia = võtmed[:]
+    õige = randint(0, len(võtmed)-1)
+    koopia.remove(võtmed[õige])
+    valed = sample(range(0, len(koopia)), 3)
+    vastusevariandid = []
+    vastusevariandid.append(võtmed[õige])
+    for i in range(3):
+        vastusevariandid.append(koopia[valed[i]])
+    shuffle(vastusevariandid)
+
+#     sõna on vaja silt2 loomisel ja failinime, kui peaks küsimuste kategooriat vahetatama (funktsioonis uus_küsimus())
+    if 'astakuks ' in võtmed:
         sõna = 'definitsiooniga'
         failinimi = 'definitsioonid.txt'
     else:
         sõna = 'teoreemiga'
         failinimi = 'teoreemid.txt'
-    
-    koopia = vastusevalikud[:]
-    õige = randint(0, len(vastusevalikud)-1)
-    koopia.remove(vastusevalikud[õige])
-    valed = sample(range(0, len(koopia)), 2)
-    
-    silt2 = Label(testiaken, text='Millise ' + sõna + ' on tegu?\n' + sõnastik[vastusevalikud[õige]])
+
+#     testiaknasse Label, kuhu tuleb küsimus
+    silt2 = Label(testiaken, text='Millise ' + sõna + ' on tegu?\n' + sõnastik[võtmed[õige]])
     silt2.pack()
+
+#     testiaknasse Frame, kuhu tulevad vastusevariandid. Proovisin valikunuppe tekitada ka tsükliga, aga siis vastuste kontrollimine ei olnud õige
     vastuste_raam = Frame(testiaken)
     vastuste_raam.pack()
     v = StringVar()
-    
-    vastus1 = ttk.Radiobutton(testiaken, text=vastusevalikud[õige], variable=v, value=vastusevalikud[õige], command=lambda: vastuse_kontroll(vastusevalikud[õige], vastusevalikud[õige]))
-    vastus2 = ttk.Radiobutton(testiaken, text=koopia[valed[0]], variable=v, value=koopia[valed[0]], command=lambda: vastuse_kontroll(koopia[valed[0]], vastusevalikud[õige]))
-    vastus3 = ttk.Radiobutton(testiaken, text=koopia[valed[1]], variable=v, value=koopia[valed[1]], command=lambda: vastuse_kontroll(koopia[valed[1]], vastusevalikud[õige]))
-    vastus1.pack()
-    vastus2.pack()
-    vastus3.pack()
-    def topeltf():
+    ttk.Radiobutton(testiaken, text=vastusevariandid[0], variable=v, value=vastusevariandid[0], command=lambda: vastuse_kontroll(vastusevariandid[0], võtmed[õige])).pack()
+    ttk.Radiobutton(testiaken, text=vastusevariandid[1], variable=v, value=vastusevariandid[1], command=lambda: vastuse_kontroll(vastusevariandid[1], võtmed[õige])).pack()
+    ttk.Radiobutton(testiaken, text=vastusevariandid[2], variable=v, value=vastusevariandid[2], command=lambda: vastuse_kontroll(vastusevariandid[2], võtmed[õige])).pack()
+    ttk.Radiobutton(testiaken, text=vastusevariandid[3], variable=v, value=vastusevariandid[3], command=lambda: vastuse_kontroll(vastusevariandid[3], võtmed[õige])).pack()
+
+    def uus_küsimus():
         testiaken.destroy()
         küsi_küsimus(failist_sõnastik(failinimi))
     def vastuse_kontroll(valik, õige_vastus):
@@ -54,7 +62,7 @@ def küsi_küsimus(sõnastik):
         else:
             tagasiside = Label(testiaken, text='Peaaegu!!! Aga siiski mitte...')
             tagasiside.pack()
-        edasi = Button(testiaken, text='Järgmine küsimus', command=lambda: topeltf())
+        edasi = Button(testiaken, text='Järgmine küsimus', command=lambda: uus_küsimus())
         edasi.pack()
         lõpeta = Button(testiaken, text='Aitab sellest testist', command=lambda: testiaken.destroy())
         lõpeta.pack()
