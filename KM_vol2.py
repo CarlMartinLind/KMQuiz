dedf09685335d0e265951078bfb6da266b71c6c5
NormanPirk <norman.pirk@gmail.com>
2019-12-12 17:04:39 +0200

    tekstidele ja nuppudele vahed, mõned tekstiparandused

--- KM_vol2.py
+++ KM_vol2.py
@@ -20,7 +20,7 @@ def failist_sõnastik(failinimi):
 def ava_küsimuste_aken(failinimi):    
     küsimuste_aken = tk.Toplevel(aken)
     küsimuste_aken.title('Kõrgema matemaatika kordamistest')
-    küsimuste_aken.geometry('600x300')
+    küsimuste_aken.geometry('600x360')
     
     küsimuste_raam = Frame(küsimuste_aken)
     küsimuste_raam.pack()
@@ -53,13 +53,13 @@ def ava_küsimuste_aken(failinimi):
             if skoor > 8:
                 tekst = 'Kogusid ' + str(skoor) + ' punkti kümnest võimalikust. Suurepärane tulemus!'
             elif skoor > 5:
-                tekst = 'Kogusid ' + str(skoor) + ' punkti kümnest võimalikust. Polegi kõige hullem!'
+                tekst = 'Kogusid ' + str(skoor) + ' punkti kümnest võimalikust. Täitsa hästi!'
             elif skoor > 3:
                 tekst = 'Kogusid ' + str(skoor) + ' punkti kümnest võimalikust. Polegi kõige hullem!'
             else:
                 tekst = 'Kogusid ' + str(skoor) + ' punkti kümnest võimalikust.'
             
-            tagasisidesilt = Label(tagasisideraam, text = tekst)
+            tagasisidesilt = Label(tagasisideraam, text = tekst, wraplength=380)
             tagasisidesilt.pack()
             
             def sulge_aken():
@@ -70,7 +70,7 @@ def ava_küsimuste_aken(failinimi):
         
         else:
             küsimuste_raam = Frame(küsimuste_aken)
-            küsimuste_raam.pack()
+            küsimuste_raam.pack(pady=20)
 
 #    siin saab järjendi (vastusevariandid), kus on 4 vastusevarianti, millest kolm on valed ja üks õige. 
             võtmed = []
@@ -88,12 +88,12 @@ def ava_küsimuste_aken(failinimi):
 
 #     sõna on vaja silt2 loomisel ja failinime, kui peaks küsimuste kategooriat vahetatama (funktsioonis uus_küsimus())
             if 'astakuks ' in võtmed:
-                sõna = 'definitsiooniga'
+                küsimus = 'Mis sobib lünka?'
             else:
-                sõna = 'teoreemiga'
+                küsimus = 'Millise teoreemiga on tegu?'
 
 #     testiaknasse Label, kuhu tuleb küsimus
-            silt2 = Label(küsimuste_raam, text = str(kordi + 1) + '. Millise ' + sõna + ' on tegu?\n' + sõnastik[võtmed[õige]], wraplength=480, justify='center')
+            silt2 = Label(küsimuste_raam, text = str(kordi + 1) + '. ' + küsimus + '\n\n' + sõnastik[võtmed[õige]], wraplength=480)
             silt2.pack()
             
 
@@ -101,8 +101,8 @@ def ava_küsimuste_aken(failinimi):
             vastuste_raam = Frame(küsimuste_raam)
             vastuste_raam.pack()
             
-            valikusilt = Label(vastuste_raam, justify = 'left')
-            valikusilt.pack(padx=10, pady=10)
+            valikusilt = Label(vastuste_raam)
+            valikusilt.pack(pady=10)
             
             v = StringVar()
             ttk.Radiobutton(valikusilt, text=vastusevariandid[0], variable=v, value=vastusevariandid[0], command=lambda: vastuse_kontroll(vastusevariandid[0], võtmed[õige], skoor)).pack(anchor=W)
@@ -141,22 +141,22 @@ def ava_küsimuste_aken(failinimi):
                 else:
                     edasi_nupu_tekst = 'Järgmine küsimus'
                 
-                edasi = ttk.Button(küsimuste_raam, text = edasi_nupu_tekst, style="danger.TButton", command=lambda: uus_küsimus(väärtus)).pack()
+                edasi = ttk.Button(küsimuste_raam, text = edasi_nupu_tekst, style="danger.TButton", command=lambda: uus_küsimus(väärtus)).pack(pady=5)
                 lõpeta = ttk.Button(küsimuste_raam, text = 'Katkesta test', style="danger.TButton", command=lambda: küsimuste_aken.destroy()).pack()   
 
 
 aken = ThemedTk(theme="radiance")
 aken.title('Kõrgem Matemaatika I')
-aken.geometry('340x200')
+aken.geometry('350x220')
 
 style = ttk.Style()
 style.configure('winnative', font="arial 11")
 
 
-silt1 = ttk.Label(aken, text='Vali küsimuste kategooria', font="arial 14").pack()
+silt1 = ttk.Label(aken, text='Vali küsimuste kategooria', font="arial 14").pack(pady=5)
 
 valik1= ttk.Button(aken, text='Teoreemid', style="danger.TButton", command=lambda: ava_küsimuste_aken('teoreemid.txt')).pack()
-valik2 = ttk.Button(aken, text='Definitsioonid', style="danger.TButton", command=lambda: ava_küsimuste_aken('definitsioonid.txt')).pack()
+valik2 = ttk.Button(aken, text='Definitsioonid', style="danger.TButton", command=lambda: ava_küsimuste_aken('definitsioonid.txt')).pack(pady=2)
 
 
 def lopp1():
@@ -165,7 +165,7 @@ def lopp1():
 def lopp2():
     silt3 = Label(aken, text='Aitäh, et küsimustele vastasid! Edukat eksamit!')
     silt3.pack()
-    sulge = ttk.Button(aken, text='Sulge programm', style="danger.TButton", command=lambda: lopp1()).pack()
+    sulge = ttk.Button(aken, text='Sulge programm', style="danger.TButton", command=lambda: lopp1()).pack(pady=5)
     
 lõpp = ttk.Button(aken, text='Lõpeta', style="danger.TButton", command=lambda: lopp2()).pack()
 
